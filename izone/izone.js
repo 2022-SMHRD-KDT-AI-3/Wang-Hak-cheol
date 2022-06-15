2d445665083073e8aaf3bcbc4c73133b9cd4f68d
이상원 <100210752+wonisang@users.noreply.github.com>
2022-06-15 14:27:35 +0900

    router커밋

--- izone/izone.js
+++ izone/izone.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 const bodyparser = require('body-parser');
 const router = require('./router/router.js');
 const ejs = require('ejs');
+const path = require("path");
 const session = require('express-session');
 const session_mysql_save = require('express-mysql-session');
 
 let DB_info = {
-    host: 'project-db-stu.ddns.net',
+    host: 'project-db-stu.ddns.net', //project-db-stu.ddns.net
     user: 'CCTV',
-    password: '0623',
     port: '3307',
-    database: 'CCTV'
+    database: 'CCTV',
+    password: '0623'
 }
 
 let session_info = new session_mysql_save(DB_info);
+
+app.set(path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  cookieParser(process.env.COOKIE_SECRET, { sameSite: "none", secure: true })
+);
 
 app.set("view engine","ejs")
 app.use(session({
@@ -26,4 +36,4 @@ app.use(session({
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(router);
-app.listen(3000);
+app.listen(3307);
--- izone/router/router.js
+++ izone/router/router.js
@@ -1,10 +1,44 @@
 const { Router } = require("express");
 const express = require("express");
+const expressMysqlSession = require("express-mysql-session");
+var path = require('path');
+var bodyParser = require('body-parser');
 const router = express.Router();
+const conn = require("../config/DB.js");
 
 // intro페이지 접근 라우터
 router.get("/intro", function (request, response) {
-    response.render("intro");
+ 
+    response.render("intro",{
+        user: request.session.user
+    });
+});
+
+router.post("/login", function (request, response) {
+    let id = request.body.id;
+    let pw = request.body.pw;
+
+    let sql = "select * from user_info where id = ? and pw = ?";
+    conn.query(sql, [id, pw], function (err, rows) {
+        console.log("로그인 성공");
+        console.log(rows.length);
+        if (rows.length > 0) {
+            request.session.user = {
+                "name": rows[0].name,
+                "org": rows[0].org,
+                "class": rows[0].class
+            }
+            response.redirect("http://127.0.0.1:3307/intro");
+            
+        } else {
+            console.log("로그인 실패");
+        }
+    });
+})
+
+router.get("/logout", function (request, response) {
+    delete request.session.user;
+    response.redirect("http://127.0.0.1:3307/intro");
 });
 
 module.exports = router;
